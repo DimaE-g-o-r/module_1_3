982c41b32d14fefdb234b5b7cf59fbd50f26b8c8
DimaE-g-o-r <egorovkog@mail.ru>
2024-09-23 19:39:17 +0500

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,6 +2,7 @@ name = "Dmitry"
 print('Name:', name, type (name))
 age = 49
 print('Age:', age)
-print('New_age:', age + 1)
+age = age + 1
+print('New_age:', age)
 is_student = 'true'
 print('Is_student:', is_student)
